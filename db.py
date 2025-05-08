16eb41ed29755e47d49bf44dcd83f2b92fc6a36e
Sven Schrodt <sven@schrodt.club>
2025-05-08 13:12:31 +0200

    Adding stuff; setting autocommit=True for sq3 connections

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from typing import Any, Self
 class Db:
     def __init__(self):
         
-        self.conn = sqlite3.connect('data/2do.db')
+        self.conn = sqlite3.connect('data/2do.db', autocommit=True)
         self.conn.row_factory = sqlite3.Row 
 
         self.crs  = self.conn.cursor()
@@ -53,7 +53,7 @@ class Task():
     
     
     def new(self, title:str, desc:str, cat_id:int, start='', end=''):
-        tpl = "INSERT INTO task (title, description, category_id, start_dt, end_dt) VALUES (?, ?, ?, ?, ?)"
+        tpl = "INSERT INTO " + self.relation + "  (title, description, category_id, start_dt, end_dt) VALUES (?, ?, ?, ?, ?)"
         self.db.res = self.db.crs.execute(tpl, [title, desc, cat_id, start, end])
     
     def get_all(self):
--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from db import Category
 from db import Task
 
 db = Db()
-cat = Category(db)
-cat.new('Foo')
 tsk = Task(db)
+
+cat = Category(db)
+cat.new('Namentanzen')
+tsk.new(title='Skinny Puppy - Remission & Bites', cat_id=4, desc='Höchstens 1000€!!', start='2025-09-01', end='2025-09-11')
+
+
+
+    
 dta = tsk.get_all();
 
 for item in dta:
     print(dict(item))
-
     
-tsk.new(title='PF _ tdsotm', cat_id=4, desc='Höchstens 55€!!', start='2025-09-01', end='2025-09-11')
-
-
-
     
+        
 dta = cat.get_all();
 
 for item in dta:
