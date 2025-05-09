8e1a42de3cffdb6c937c05a7e37d26314c3fa0f3
Sven Schrodt <sven@schrodt.club>
2025-05-09 13:24:12 +0200

    Adding external resources fro HTML stuff

--- foo.php
+++ foo.php
@@ -1,4 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Eintrag Todo</title>
+  <link rel='stylesheet' href='form.css'>
+
+</html>
+
+<body>
+    <h2>Achtung: dies ist (projekt-) extern!</h2>
+    <section class="main">
 <pre>
 <?php
 
 var_dump($_REQUEST);
+
+?>
+</pre>
+</section>
+</body>
+</html>
